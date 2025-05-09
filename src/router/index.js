46e68256688f6ba7963793ba9ab55f02cd811b8d
las.engenharia <luamso@gmail.com>
2025-05-09 19:30:19 -0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'; // ou createWebHashHistory se for sua preferência
 import { waitForAuthReady } from '../authReady';
+
 
 import DashboardView from '../views/DashboardView.vue';
 import ProjetosView from '../views/ProjetosView.vue';
@@ -10,11 +10,11 @@ import TiposItemView from '../views/Fornecedores.vue';
 import ItensView from '../views/ItensView.vue';
 import ExecucaoView from '../views/ExecucaoView.vue';
 import DiarioObraView from '../views/DiarioObraView.vue';
-import CronogramaView from '../views/CronogramaView.vue';
-import CronogramaCalendario from '../views/CronogramaView.vue';
 import RelatoriosView from '../views/RelatoriosView.vue';
 import ConfiguracoesView from '../views/ConfiguracoesView.vue';
 import TarefasView from '../views/TarefasView.vue';
+import TiposItemView from '../views/Fornecedores.vue'; // nome do import está confuso
+
 
 
 
@@ -32,6 +32,7 @@ const routes = [
   { path: '/relatorios', component: RelatoriosView, meta: { requiresAuth: true } },
   { path: '/configuracoes', component: ConfiguracoesView, meta: { requiresAuth: true } },
   { path: '/tarefas', component: TarefasView, meta: { requiresAuth: true } },
+{ path: '/fornecedores', component: FornecedoresView, meta: { requiresAuth: true } },
 
 ];
 
@@ -42,48 +43,3 @@ const router = createRouter({
 });
 
 export default router;
-=======
-import { createRouter, createWebHistory } from 'vue-router';
-import { waitForAuthReady } from '../authReady';
-
-import DashboardView from '../views/DashboardView.vue';
-import ProjetosView from '../views/ProjetosView.vue';
-import EtapasView from '../views/EtapasView.vue';
-import FuncionariosView from '../views/FuncionariosView.vue';
-import TiposItemView from '../views/Fornecedores.vue';
-import ItensView from '../views/ItensView.vue';
-import ExecucaoView from '../views/ExecucaoView.vue';
-import DiarioObraView from '../views/DiarioObraView.vue';
-import CronogramaView from '../views/CronogramaView.vue';
-import CronogramaCalendario from '../views/CronogramaView.vue';
-import RelatoriosView from '../views/RelatoriosView.vue';
-import ConfiguracoesView from '../views/ConfiguracoesView.vue';
-import TarefasView from '../views/TarefasView.vue';
-
-
-
-const routes = [
-  { path: '/', redirect: '/dashboard' },
-  { path: '/dashboard', component: DashboardView, meta: { requiresAuth: true } },
-  { path: '/projetos', component: ProjetosView, meta: { requiresAuth: true } },
-  { path: '/etapas', component: EtapasView, meta: { requiresAuth: true } },
-  { path: '/funcionarios', component: FuncionariosView, meta: { requiresAuth: true } },
-  { path: '/fornecedores', component: TiposItemView, meta: { requiresAuth: true } },
-  { path: '/itens', component: ItensView, meta: { requiresAuth: true } },
-  { path: '/execucao', component: ExecucaoView, meta: { requiresAuth: true } },
-  { path: '/diario-obra', component: DiarioObraView, meta: { requiresAuth: true } },
-  { path: '/cronograma', component: CronogramaView, meta: { requiresAuth: true } },
-  { path: '/relatorios', component: RelatoriosView, meta: { requiresAuth: true } },
-  { path: '/configuracoes', component: ConfiguracoesView, meta: { requiresAuth: true } },
-  { path: '/tarefas', component: TarefasView, meta: { requiresAuth: true } },
-
-];
-
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
-
-export default router;
->>>>>>> 9396cd378334692f8c2f504fe0827aa082427823
