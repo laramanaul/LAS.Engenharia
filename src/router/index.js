bb0af2e0bcb5cdecd192df0aa8bab06c939ce109
las.engenharia <luamso@gmail.com>
2025-05-09 21:17:35 -0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import DashboardView from '../views/DashboardView.vue';
 import ProjetosView from '../views/ProjetosView.vue';
 import EtapasView from '../views/EtapasView.vue';
 import FuncionariosView from '../views/FuncionariosView.vue';
-import TiposItemView from '../views/Fornecedores.vue';
+import FornecedoresView from '../views/Fornecedores.vue';
 import ItensView from '../views/ItensView.vue';
 import ExecucaoView from '../views/ExecucaoView.vue';
 import DiarioObraView from '../views/DiarioObraView.vue';
@@ -24,15 +24,14 @@ const routes = [
   { path: '/projetos', component: ProjetosView, meta: { requiresAuth: true } },
   { path: '/etapas', component: EtapasView, meta: { requiresAuth: true } },
   { path: '/funcionarios', component: FuncionariosView, meta: { requiresAuth: true } },
-  { path: '/fornecedores', component: FornecedoresView, meta: { requiresAuth: true } },
   { path: '/itens', component: ItensView, meta: { requiresAuth: true } },
   { path: '/execucao', component: ExecucaoView, meta: { requiresAuth: true } },
   { path: '/diario-obra', component: DiarioObraView, meta: { requiresAuth: true } },
-  { path: '/cronograma', component: CronogramaView, meta: { requiresAuth: true } },
   { path: '/relatorios', component: RelatoriosView, meta: { requiresAuth: true } },
   { path: '/configuracoes', component: ConfiguracoesView, meta: { requiresAuth: true } },
   { path: '/tarefas', component: TarefasView, meta: { requiresAuth: true } },
-{ path: '/tiposItem', component: TiposItemViewView, meta: { requiresAuth: true } },
+{ path: '/fornecedores', component: FornecedoresView, meta: { requiresAuth: true } },
+
 
 ];
 
